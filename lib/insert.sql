505ce884e32162175efdc5cfdec761551d6ba321
Drake Damon <dddamon06@gmail.com>
2024-11-11 14:37:07 -0500

    completed bears lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO  bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "M", "brown", "nice", True);
+INSERT INTO  bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 3, "M", "black", "mean", True);
+INSERT INTO  bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 4, "F", "tan", "nice", True);
+INSERT INTO  bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 5, "M", "black", "nice", True);
+INSERT INTO  bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 6, "F", "brown", "mean", False);
+INSERT INTO  bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 7, "M", "white", "nice", True);
+INSERT INTO  bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 8, "F", "brown", "mean", True);
+INSERT INTO  bears (name, age, sex, color, temperament, alive) VALUES (NULL, 9, "F", "yellow", "nice", True);
